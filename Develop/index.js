61ef8467ab1269988aaceca62afe361fa60f9e6a
Sebastian <sebastianalvarado246801@gmail.com>
2021-05-23 11:00:02 -0500

    removed unecessary code

--- Develop/index.js
+++ Develop/index.js
@@ -118,32 +118,7 @@ const questions = [
         }
    }
 ];
-// const collectInputs = async (inputs = []) => {
-//     const prompts = [
-//       {
-//         type: 'input',
-//         name: 'inputValue',
-//         message: 'Enter some input: '
-//       },
-//       {
-//         type: 'confirm',
-//         name: 'again',
-//         message: 'Enter another input? ',
-//         default: true
-//       }
-//     ];
-  
-//     const { again, ...answers } = await inquirer.prompt(prompts);
-//     const newInputs = [...inputs, answers];
-//     return again ? collectInputs(newInputs) : newInputs;
-//   };
-  
-//   const main = async () => {
-//     const inputs = await collectInputs();
-//     console.log(inputs);
-//   };
-  
-//   main();
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), data)
